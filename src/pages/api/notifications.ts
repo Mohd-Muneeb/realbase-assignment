413dcd5dbaf716927a5499720d90c84effd6a13d
Mohd-Muneeb <mohdmuneeb1307@gmail.com>
2023-07-18 23:42:50 +0530

    Finishing changes

--- src/pages/api/notifications.ts
+++ src/pages/api/notifications.ts
@@ -1,7 +1,15 @@
-import { type NextApiRequest, type NextApiResponse } from "next";
-// import data from "~/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "~/server/db";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  // res.status(200).json(data);
-  res.status(200);
+export default async function Acknowledged(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).json("Invalid Method");
+  }
+
+  const data = await prisma.notificationType.findMany();
+
+  return res.status(400).json(data);
 }
